7ee9c56d9f29a6b0a5bec161bfe313b02a724683
IDoNotUnderstandCodingAtAll <38897385+sebscamcoder@users.noreply.github.com>
2021-12-18 18:27:55 -0600

    Create main.py
    I will update this frequently. be sure to tell me issues/suggestions !

---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+import random
+import string
+import time
+import threading
+print ("""
+Ayo who put the fall guy here!?!?!!?!?!?!!?!?
+
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠉⠉⠉⠉⠉⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⢀⣠⣶⣶⣶⣶⣤⡀⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡏⠄⠄⣾⡿⢿⣿⣿⡿⢿⣿⡆⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄⠄⢿⣇⣸⣿⣿⣇⣸⡿⠃⠄⠄⠸⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⠄⠄⠉⠛⠛⠛⠛⠉⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢿⣿⣿⣿
+⣿⣿⣿⡟⠄⠄⠄⠠⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢿⣿⣿
+⣿⣿⡟⠄⠄⠄⢠⣆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣧⠄⠄⠄⠈⢿⣿
+⣿⣿⡇⠄⠄⠄⣾⣿⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⣿⣧⠄⠄⠄⠘⣿
+⣿⣿⣇⠄⣰⣶⣿⣿⣿⣦⣀⡀⠄⠄⠄⠄⠄⠄⠄⢀⣠⣴⣿⣿⣿⣶⣆⠄⢀⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⠄⢸⣿⠇⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣴⣾⣿⣶⣤⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀
+Gen script from https://github.com/Sadtrxsh/Realm-Invite-Checker/blob/master/gen.py
+Checker script by https://github.com/yeeterlol/RCB
+The chance that this will actually find a code is so rare that if you do tell me on github
+""")
+time.sleep(4)
+print("starting...")
+time.sleep(1)
+def SendRequest():
+  while True:
+        # printing letters
+        code = ('').join(random.choices(string.ascii_letters + string.digits, k=11))
+        link = f"https://open.minecraft.net/pocket/realms/invite/{code}"
+        r = requests.get(link)
+
+        if r.status_code == 404:
+            print(f"This code was invalid!    {code}")
+        elif r.status_code == 200:
+            print(f"HOLY SHIT YOU GOT ONE Valid | {link}")
+
+        else:
+            print(f"This link got an error!   {link}")
+
+
+
+threads = []
+
+for i in range (5):
+  t = threading.Thread(target=SendRequest)
+  t.daemon = True
+  threads.append(t)
+
+for i in range(5):
+  threads[i].start()
+
+for i in range(5):
+  threads[i].join()
